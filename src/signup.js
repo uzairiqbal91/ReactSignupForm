579e0ac21b0c76a5e88c7dda6917cda17c9768c3
Uzair Iqbal <Uzair Iqbal>
2017-04-09 17:41:09 +0500

    sign up form validation

--- src/signup.js
+++ src/signup.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import  './SignUp_css.css';
 import TableView from './TableView';
+import Button from 'react-bootstrap/lib/Button';
 
 class SignUp extends Component{
 constructor(){
@@ -13,7 +14,9 @@ constructor(){
         password:'',
         cPassword:''
     },
-    fields:[]
+    
+    fields:[],
+    error : "this is error"
   }
 }
 
@@ -44,20 +47,43 @@ inputChange(changeValue,event){
     });
 
 }
+validEmail(value){
+// regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(value);
+}
 
 render(){
 return(
    <div>
-      <form className="signUp_css">  
+      <form className="signUp_css" onSubmit={this.successMessage.bind(this)} name="myForm">  
             <br></br><br></br>
             <label><b>First Name</b></label>
-            <input className="inputField" type="text" value={this.state.myInfo.fName} onChange={this.inputChange.bind(this,"fName")}/>
+            <input className="inputField" 
+                    type="text" 
+                    value={this.state.myInfo.fName} 
+                    onChange={this.inputChange.bind(this,"fName")}
+                    placeholder="First Name"
+                    minLength={6}
+                    maxLength={10}
+                    required={true}
+                    style={this.state.myInfo.fName.length  > 6 ? {borderColor : 'green'} : {}}/>
+
             <br></br><br></br>
             <label><b> Last Name</b></label>
             <input type="text" value={this.state.myInfo.lName} onChange={this.inputChange.bind(this,"lName")}/>
             <br></br><br></br>
             <label><b>E-mail</b></label>
-            <input type="text" value={this.state.myInfo.mobileNo} onChange={this.inputChange.bind(this,"e_mail")}/>
+            <input type="email" value={this.state.myInfo.mobileNo}
+                    placeholder="Email"
+                    minLength={6}
+                    validate={this.validEmail}
+                    maxLength={25}
+                    required={true}
+                    errorMessage="Email is invalid"
+                    emptyMessage="Email is required"
+                    onChange={this.inputChange.bind(this,"e_mail")}/>
+                    <p style={this.state.myInfo.e_mail.length < 5 ? {display:'none'} : {}  }>{this.state.error}</p>
             <br></br><br></br>
             <label><b>New password</b></label>
             <input type="password" value={this.state.myInfo.password} onChange={this.inputChange.bind(this,"password")}/>
@@ -66,7 +92,7 @@ return(
             <input type="password" value={this.state.cPassword} onChange={this.inputChange.bind(this,"cPassword")}/>
             <br></br><br></br>
           
-            <button onClick={this.successMessage.bind(this)}><b>Create Account</b></button>
+            <Button bsStyle="info" onClick={this.successMessage.bind(this)} ><b>Create Account</b></Button>
        </form>
 
         <div>
